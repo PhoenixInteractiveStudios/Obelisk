c27edab51f92f561aea22e124e5ca7a1ea63f565
TurtleException <turtle.exception@eldritch.de>
2023-12-26 22:33:37 +0100

    Add version resource

--- obelisk-server/src/main/java/org/burrow_studios/obelisk/server/Main.java
+++ obelisk-server/src/main/java/org/burrow_studios/obelisk/server/Main.java
@@ -1,5 +1,7 @@
 package org.burrow_studios.obelisk.server;
 
+import org.burrow_studios.obelisk.common.ResourceTools;
+
 import java.io.File;
 import java.net.URISyntaxException;
 
@@ -7,6 +9,9 @@ public class Main {
     static {
         System.out.print("Starting Obelisk Server");
     }
+
+    /** Static application version. */
+    public static final String VERSION = ResourceTools.get(Main.class).getVersion();
 
     /** Directory in which the JAR ist located. */
     public static final File DIR;
@@ -23,6 +28,8 @@ public class Main {
 
     /** JVM entrypoint */
     public static void main(String[] args) {
-
+        if (VERSION == null)
+            throw new AssertionError("Unknown version");
+        System.out.printf(" version %s...%n", VERSION);
     }
 }
