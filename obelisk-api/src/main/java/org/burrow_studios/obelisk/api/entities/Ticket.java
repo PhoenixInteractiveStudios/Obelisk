4436b603f729c94aa4ebe7944c17400317c394ab
TurtleException <turtle.exception@eldritch.de>
2023-12-27 02:01:27 +0100

    Create Ticket

---
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/api/entities/Ticket.java
@@ -0,0 +1,28 @@
+package org.burrow_studios.obelisk.api.entities;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+public interface Ticket extends Turtle {
+    @Nullable String getTitle();
+
+    @NotNull State getState();
+
+    @NotNull List<String> getTags();
+
+    @NotNull Set<Locale> getUserIds();
+
+    @NotNull Set<User> getUsers();
+
+    enum State {
+        OPEN,
+        RESOLVED,
+        FROZEN,
+        CLOSED,
+        DRAFT
+    }
+}
