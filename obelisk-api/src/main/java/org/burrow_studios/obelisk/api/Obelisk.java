7c8a1f2ef73419d987ce578db18b8983da8427b2
TurtleException <turtle.exception@eldritch.de>
2023-12-28 19:13:36 +0100

    Implement Obelisk TurtleContainer methods

--- obelisk-api/src/main/java/org/burrow_studios/obelisk/api/Obelisk.java
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/api/Obelisk.java
@@ -1,5 +1,36 @@
 package org.burrow_studios.obelisk.api;
 
-public interface Obelisk {
+import org.burrow_studios.obelisk.api.entities.*;
+import org.burrow_studios.obelisk.api.entities.issue.Board;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Set;
+
+public interface Obelisk {
+    @NotNull Set<? extends Turtle> getEntities();
+
+    @Nullable Turtle getEntity(long id);
+
+    /* - GENERAL - */
+
+    @NotNull Set<? extends Group> getGroups();
+
+    @Nullable Group getGroup(long id);
+
+    @NotNull Set<? extends Project> getProjects();
+
+    @Nullable Project getProject(long id);
+
+    @NotNull Set<? extends Ticket> getTickets();
+
+    @Nullable Ticket getTicket(long id);
+
+    @NotNull Set<? extends User> getUsers();
+
+    @Nullable User getUser(long id);
+
+    @NotNull Set<? extends Board> getBoards();
+
+    @Nullable Board getBoard(long id);
 }
--- obelisk-api/src/main/java/org/burrow_studios/obelisk/internal/ObeliskImpl.java
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/internal/ObeliskImpl.java
@@ -1,17 +1,29 @@
 package org.burrow_studios.obelisk.internal;
 
 import org.burrow_studios.obelisk.api.Obelisk;
+import org.burrow_studios.obelisk.api.entities.*;
+import org.burrow_studios.obelisk.api.entities.issue.Board;
+import org.burrow_studios.obelisk.internal.cache.TurtleCache;
+import org.burrow_studios.obelisk.internal.entities.TurtleImpl;
 import org.burrow_studios.obelisk.internal.event.EventHandlerImpl;
 import org.burrow_studios.obelisk.internal.net.NetworkHandler;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ObeliskImpl implements Obelisk {
     private final EventHandlerImpl eventHandler;
     private final NetworkHandler   networkHandler;
 
+    private final TurtleCache<TurtleImpl> entityCache;
+
     public ObeliskImpl() {
         this.eventHandler   = new EventHandlerImpl(this);
         this.networkHandler = new NetworkHandler(this);
+
+        this.entityCache = new TurtleCache<>();
     }
 
     public @NotNull EventHandlerImpl getEventHandler() {
@@ -20,5 +32,82 @@ public class ObeliskImpl implements Obelisk {
 
     public @NotNull NetworkHandler getNetworkHandler() {
         return networkHandler;
+    }
+
+    /* - ENTITIES - */
+
+    @Override
+    public @NotNull Set<? extends Turtle> getEntities() {
+        return Set.copyOf(this.entityCache);
+    }
+
+    @Override
+    public @Nullable Turtle getEntity(long id) {
+        return this.entityCache.get(id);
+    }
+
+    @Override
+    public @NotNull Set<? extends Group> getGroups() {
+        return this.entityCache.stream()
+                .filter(Group.class::isInstance)
+                .map(Group.class::cast)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public @Nullable Group getGroup(long id) {
+        return this.entityCache.get(id, Group.class);
+    }
+
+    @Override
+    public @NotNull Set<? extends Project> getProjects() {
+        return this.entityCache.stream()
+                .filter(Project.class::isInstance)
+                .map(Project.class::cast)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public @Nullable Project getProject(long id) {
+        return this.entityCache.get(id, Project.class);
+    }
+
+    @Override
+    public @NotNull Set<? extends Ticket> getTickets() {
+        return this.entityCache.stream()
+                .filter(Ticket.class::isInstance)
+                .map(Ticket.class::cast)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public @Nullable Ticket getTicket(long id) {
+        return this.entityCache.get(id, Ticket.class);
+    }
+
+    @Override
+    public @NotNull Set<? extends User> getUsers() {
+        return this.entityCache.stream()
+                .filter(User.class::isInstance)
+                .map(User.class::cast)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public @Nullable User getUser(long id) {
+        return this.entityCache.get(id, User.class);
+    }
+
+    @Override
+    public @NotNull Set<? extends Board> getBoards() {
+        return this.entityCache.stream()
+                .filter(Board.class::isInstance)
+                .map(Board.class::cast)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public @Nullable Board getBoard(long id) {
+        return this.entityCache.get(id, Board.class);
     }
 }
