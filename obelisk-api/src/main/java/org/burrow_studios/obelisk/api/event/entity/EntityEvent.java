6fde761e293e2eb97c4afa6b1a512676b44d667e
TurtleException <turtle.exception@eldritch.de>
2023-12-29 18:22:42 +0100

    Create generic entity events

---
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/api/event/entity/EntityEvent.java
@@ -0,0 +1,18 @@
+package org.burrow_studios.obelisk.api.event.entity;
+
+import org.burrow_studios.obelisk.api.entities.Turtle;
+import org.burrow_studios.obelisk.api.event.AbstractEvent;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class EntityEvent<E extends Turtle> extends AbstractEvent {
+    protected final @NotNull E entity;
+
+    protected EntityEvent(@NotNull E entity) {
+        super(entity.getAPI());
+        this.entity = entity;
+    }
+
+    public final @NotNull E getEntity() {
+        return this.entity;
+    }
+}
