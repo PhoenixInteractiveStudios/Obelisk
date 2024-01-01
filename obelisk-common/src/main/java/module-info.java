834e366f34d20c5f8fd87518a5be2745aec9bd18
TurtleException <turtle.exception@eldritch.de>
2024-01-01 04:15:17 +0100

    Fix dependency issue in obelisk-common

--- obelisk-common/src/main/java/module-info.java
+++ obelisk-common/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module org.burrow_studios.obelisk.common {
+    requires org.jetbrains.annotations;
     exports org.burrow_studios.obelisk.common;
     exports org.burrow_studios.obelisk.common.function;
 }
