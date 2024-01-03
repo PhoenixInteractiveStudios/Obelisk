e5ad6dabbe98da1390ac476cd193a4aaa07c592d
TurtleException <turtle.exception@eldritch.de>
2024-01-03 03:06:37 +0100

    Move non-specific request handling to APIHandler

--- obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/APIHandler.java
+++ obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/APIHandler.java
@@ -3,9 +3,12 @@ package org.burrow_studios.obelisk.server.net.http;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.burrow_studios.obelisk.server.net.NetworkHandler;
+import org.burrow_studios.obelisk.server.net.http.exceptions.ForbiddenException;
+import org.burrow_studios.obelisk.server.net.http.exceptions.IllegalMethodException;
+import org.burrow_studios.obelisk.server.net.http.exceptions.UnauthorizedException;
 import org.jetbrains.annotations.NotNull;
 
-import java.net.URI;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -44,8 +47,7 @@ public abstract class APIHandler {
         return this.networkHandler;
     }
 
-    protected final @NotNull Endpoint getEndpoint(@NotNull Method method, @NotNull URI uri) {
-        final String   path     = uri.getPath();
+    protected final @NotNull Endpoint getEndpoint(@NotNull Method method, @NotNull String path) {
         final String[] segments = path.substring(1).split("/");
 
         for (Endpoint endpoint : handlers.keySet()) {
@@ -55,10 +57,44 @@ public abstract class APIHandler {
             return endpoint;
         }
 
-        return new Endpoint(method, uri.getPath());
+        return new Endpoint(method, path);
     }
 
     protected final @NotNull EndpointHandler getHandler(@NotNull Endpoint endpoint) {
         return this.handlers.getOrDefault(endpoint, this.defaultHandler);
+    }
+
+    protected final @NotNull Response handle(@NotNull Map<String, String> headers, @NotNull Method method, @NotNull String path) {
+        final Endpoint        endpoint = this.getEndpoint(method, path);
+        final EndpointHandler handler  = this.getHandler(endpoint);
+
+        final Request request = new Request(
+                this,
+                endpoint,
+                headers
+        );
+
+        final ResponseBuilder builder = new ResponseBuilder();
+        final ResponseBuilder error   = new ResponseBuilder();
+
+        try {
+            handler.handle(request, builder);
+
+            return builder.build();
+        } catch (UnauthorizedException e) {
+            error.setCode(401);
+
+            error.setHeader("WWW-Authenticate", "Bearer");
+        } catch (ForbiddenException e) {
+            error.setCode(403);
+        } catch (IllegalMethodException e) {
+            error.setCode(405);
+
+            error.setHeader("Allow", String.join(", ", e.getAllowedStr()));
+        } catch (Exception e) {
+            error.setCode(500);
+        }
+
+        return error.build();
     }
 }
--- obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/server/SunServerImpl.java
+++ obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/server/SunServerImpl.java
@@ -5,10 +5,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.burrow_studios.obelisk.server.net.NetworkHandler;
-import org.burrow_studios.obelisk.server.net.http.*;
-import org.burrow_studios.obelisk.server.net.http.exceptions.ForbiddenException;
-import org.burrow_studios.obelisk.server.net.http.exceptions.IllegalMethodException;
-import org.burrow_studios.obelisk.server.net.http.exceptions.UnauthorizedException;
+import org.burrow_studios.obelisk.server.net.http.APIHandler;
+import org.burrow_studios.obelisk.server.net.http.Method;
+import org.burrow_studios.obelisk.server.net.http.Response;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -44,46 +43,13 @@ public class SunServerImpl extends APIHandler implements HttpHandler {
         final String methodStr = exchange.getRequestMethod();
         final Method method    = Method.valueOf(methodStr);
 
-        final Endpoint        endpoint = this.getEndpoint(method, uri);
-        final EndpointHandler handler  = this.getHandler(endpoint);
-
         final Map<String, String> headers = exchange.getRequestHeaders().entrySet().stream()
                 // combine duplicate header fields into one (separated by comma)
                 .map(entry -> Map.entry(entry.getKey(), String.join(", ", entry.getValue())))
                 // collect as Map<String, String>
                 .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 
-        final Request request = new Request(
-                this,
-                endpoint,
-                headers
-        );
-
-        final ResponseBuilder builder = new ResponseBuilder();
-
-        try {
-            handler.handle(request, builder);
-        } catch (UnauthorizedException e) {
-            builder.clear();
-            builder.setCode(401);
-
-            builder.setHeader("WWW-Authenticate", "Bearer");
-        } catch (ForbiddenException e) {
-            builder.clear();
-            builder.setCode(403);
-        } catch (IllegalMethodException e) {
-            builder.clear();
-            builder.setCode(405);
-
-            builder.setHeader("Allow", String.join(", ", e.getAllowedStr()));
-        } catch (Exception e) {
-            builder.clear();
-            builder.setCode(500);
-        }
-
-        // send response
-
-        final Response response = builder.build();
+        final Response response = this.handle(headers, method, uri.getPath());
 
         final Headers responseHeaders = exchange.getResponseHeaders();
         response.headers().forEach(responseHeaders::add);
