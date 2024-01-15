565c7a51355dd9d48b3eba5b6dfd3523c76fb021
TurtleException <turtle.exception@eldritch.de>
2024-01-16 00:54:03 +0100

    Rewrite GatewayOpcodes

--- obelisk-core/src/main/java/org/burrow_studios/obelisk/core/event/gateway/GatewayOpcodes.java
+++ obelisk-core/src/main/java/org/burrow_studios/obelisk/core/event/gateway/GatewayOpcodes.java
@@ -1,47 +1,89 @@
 package org.burrow_studios.obelisk.core.event.gateway;
 
+import org.burrow_studios.obelisk.api.event.GatewayEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.board.BoardCreateEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.board.BoardDeleteEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.board.BoardUpdateGroupEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.board.BoardUpdateTitleEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.issue.*;
+import org.burrow_studios.obelisk.api.event.entity.board.tag.TagCreateEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.tag.TagDeleteEvent;
+import org.burrow_studios.obelisk.api.event.entity.board.tag.TagUpdateNameEvent;
+import org.burrow_studios.obelisk.api.event.entity.group.GroupCreateEvent;
+import org.burrow_studios.obelisk.api.event.entity.group.GroupDeleteEvent;
+import org.burrow_studios.obelisk.api.event.entity.group.GroupUpdateMembersEvent;
+import org.burrow_studios.obelisk.api.event.entity.group.GroupUpdateNameEvent;
+import org.burrow_studios.obelisk.api.event.entity.project.*;
+import org.burrow_studios.obelisk.api.event.entity.ticket.*;
+import org.burrow_studios.obelisk.api.event.entity.user.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public final class GatewayOpcodes {
-    public static final int GROUP_CREATE_EVENT          = 100;
-    public static final int GROUP_DELETE_EVENT          = 101;
-    public static final int GROUP_UPDATE_MEMBERS_EVENT  = 102;
-    public static final int GROUP_UPDATE_NAME_EVENT     = 103;
-    public static final int GROUP_UPDATE_POSITION_EVENT = 104;
+    private static final GatewayOpcodes INSTANCE = new GatewayOpcodes()
+            // GROUP
+            .add(100, GroupCreateEvent.class)
+            .add(101, GroupDeleteEvent.class)
+            .add(102, GroupUpdateMembersEvent.class)
+            .add(103, GroupUpdateNameEvent.class)
+            .add(104, GroupUpdateNameEvent.class)
+            // PROJECT
+            .add(110, ProjectCreateEvent.class)
+            .add(111, ProjectDeleteEvent.class)
+            .add(112, ProjectUpdateMembersEvent.class)
+            .add(113, ProjectUpdateStateEvent.class)
+            .add(114, ProjectUpdateTimingsEvent.class)
+            .add(115, ProjectUpdateTitleEvent.class)
+            // TICKET
+            .add(120, TicketCreateEvent.class)
+            .add(121, TicketDeleteEvent.class)
+            .add(122, TicketUpdateStateEvent.class)
+            .add(123, TicketUpdateTagsEvent.class)
+            .add(124, TicketUpdateTitleEvent.class)
+            .add(125, TicketUpdateUsersEvent.class)
+            // USER
+            .add(130, UserCreateEvent.class)
+            .add(131, UserDeleteEvent.class)
+            .add(132, UserUpdateDiscordIdsEvent.class)
+            .add(133, UserUpdateMinecraftIdsEvent.class)
+            .add(134, UserUpdateNameEvent.class)
+            // BOARD
+            .add(140, BoardCreateEvent.class)
+            .add(141, BoardDeleteEvent.class)
+            .add(142, BoardUpdateGroupEvent.class)
+            .add(143, BoardUpdateTitleEvent.class)
+            // ISSUE
+            .add(150, IssueCreateEvent.class)
+            .add(151, IssueDeleteEvent.class)
+            .add(152, IssueUpdateAssigneesEvent.class)
+            .add(153, IssueUpdateStateEvent.class)
+            .add(154, IssueUpdateTagsEvent.class)
+            .add(155, IssueUpdateTagsEvent.class)
+            // TAG
+            .add(160, TagCreateEvent.class)
+            .add(161, TagDeleteEvent.class)
+            .add(162, TagUpdateNameEvent.class)
+            ;
 
-    public static final int PROJECT_CREATE_EVENT         = 110;
-    public static final int PROJECT_DELETE_EVENT         = 111;
-    public static final int PROJECT_UPDATE_MEMBERS_EVENT = 112;
-    public static final int PROJECT_UPDATE_STATE_EVENT   = 113;
-    public static final int PROJECT_UPDATE_TIMINGS_EVENT = 114;
-    public static final int PROJECT_UPDATE_TITLE_EVENT   = 115;
+    public static Class<? extends GatewayEvent> get(int code) {
+        return INSTANCE.opcodeToEvent.get(code);
+    }
 
-    public static final int TICKET_CREATE_EVENT       = 120;
-    public static final int TICKET_DELETE_EVENT       = 121;
-    public static final int TICKET_UPDATE_STATE_EVENT = 123;
-    public static final int TICKET_UPDATE_TAGS_EVENT  = 124;
-    public static final int TICKET_UPDATE_TITLE_EVENT = 125;
-    public static final int TICKET_UPDATE_USERS_EVENT = 126;
+    public static int get(@NotNull Class<? extends GatewayEvent> type) {
+        return INSTANCE.eventToOpcode.get(type);
+    }
 
-    public static final int USER_CREATE_EVENT               = 130;
-    public static final int USER_DELETE_EVENT               = 131;
-    public static final int USER_UPDATE_DISCORD_IDS_EVENT   = 132;
-    public static final int USER_UPDATE_MINECRAFT_IDS_EVENT = 133;
-    public static final int USER_UPDATE_NAME_EVENT          = 134;
+    /* - - - */
 
-    public static final int BOARD_CREATE_EVENT       = 140;
-    public static final int BOARD_DELETE_EVENT       = 141;
-    public static final int BOARD_UPDATE_GROUP_EVENT = 143;
-    public static final int BOARD_UPDATE_TITLE_EVENT = 145;
-
-    public static final int ISSUE_CREATE_EVENT           = 150;
-    public static final int ISSUE_DELETE_EVENT           = 151;
-    public static final int ISSUE_UPDATE_ASSIGNEES_EVENT = 152;
-    public static final int ISSUE_UPDATE_STATE_EVENT     = 153;
-    public static final int ISSUE_UPDATE_TAGS_EVENT      = 154;
-    public static final int ISSUE_UPDATE_TITLE_EVENT     = 155;
-
-    public static final int TAG_CREATE_EVENT      = 160;
-    public static final int TAG_DELETE_EVENT      = 161;
-    public static final int TAG_UPDATE_NAME_EVENT = 163;
+    private final ConcurrentHashMap<Integer, Class<? extends GatewayEvent>> opcodeToEvent = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Class<? extends GatewayEvent>, Integer> eventToOpcode = new ConcurrentHashMap<>();
 
     private GatewayOpcodes() { }
+
+    private GatewayOpcodes add(int code, @NotNull Class<? extends GatewayEvent> type) {
+        this.opcodeToEvent.put(code, type);
+        this.eventToOpcode.put(type, code);
+        return this;
+    }
 }
