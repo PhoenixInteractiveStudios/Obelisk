e00a13156a5aed668b26925f321bd9c94801edb3
TurtleException <turtle.exception@eldritch.de>
2024-01-16 05:37:18 +0100

    Implement SimpleSymmetricEncryption

--- obelisk-core/src/main/java/org/burrow_studios/obelisk/core/net/socket/crypto/EncryptionException.java
+++ obelisk-core/src/main/java/org/burrow_studios/obelisk/core/net/socket/crypto/EncryptionException.java
@@ -1,5 +1,7 @@
 package org.burrow_studios.obelisk.core.net.socket.crypto;
 
 public class EncryptionException extends Exception {
-
+    public EncryptionException(Throwable cause) {
+        super(cause);
+    }
 }
