78ae43903a717c26e60940d5577bd2167a742f07
TurtleException <turtle.exception@eldritch.de>
2024-01-18 06:11:33 +0100

    Move ticket request processing to TicketHandler

--- obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/handlers/TicketHandler.java
+++ obelisk-server/src/main/java/org/burrow_studios/obelisk/server/net/http/handlers/TicketHandler.java
@@ -3,9 +3,12 @@ package org.burrow_studios.obelisk.server.net.http.handlers;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.burrow_studios.obelisk.server.db.DatabaseException;
-import org.burrow_studios.obelisk.server.db.NoSuchEntryException;
-import org.burrow_studios.obelisk.server.moderation.ModerationService;
+import org.burrow_studios.obelisk.api.entities.Ticket;
+import org.burrow_studios.obelisk.core.ObeliskImpl;
+import org.burrow_studios.obelisk.core.entities.action.ticket.TicketBuilderImpl;
+import org.burrow_studios.obelisk.core.entities.action.ticket.TicketModifierImpl;
+import org.burrow_studios.obelisk.core.entities.impl.TicketImpl;
+import org.burrow_studios.obelisk.core.entities.impl.UserImpl;
 import org.burrow_studios.obelisk.server.net.NetworkHandler;
 import org.burrow_studios.obelisk.server.net.http.Request;
 import org.burrow_studios.obelisk.server.net.http.ResponseBuilder;
@@ -15,7 +18,10 @@ import org.burrow_studios.obelisk.server.net.http.exceptions.InternalServerError
 import org.burrow_studios.obelisk.server.net.http.exceptions.NotFoundException;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 public class TicketHandler {
     private final @NotNull NetworkHandler networkHandler;
@@ -26,7 +32,8 @@ public class TicketHandler {
 
     public void onGetAll(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
         JsonArray json = new JsonArray();
-        getModerationService().getTickets().forEach(json::add);
+        for (long id : getAPI().getTickets().getIdsAsImmutaleSet())
+            json.add(id);
 
         response.setCode(200);
         response.setBody(json);
@@ -35,18 +42,12 @@ public class TicketHandler {
     public void onGet(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
         final long ticketId = request.getSegmentLong(1);
 
-        final JsonObject ticket;
-
-        try {
-            ticket = getModerationService().getTicket(ticketId);
-        } catch (NoSuchEntryException e) {
+        final TicketImpl ticket = getAPI().getTicket(ticketId);
+        if (ticket == null)
             throw new NotFoundException();
-        } catch (DatabaseException e) {
-            throw new InternalServerErrorException();
-        }
 
         response.setCode(200);
-        response.setBody(ticket);
+        response.setBody(ticket.toJson());
     }
 
     public void onCreate(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
@@ -59,9 +60,42 @@ public class TicketHandler {
         final JsonObject json = body.get();
         final JsonObject result;
 
+        final TicketBuilderImpl builder;
+
         try {
-            result = getModerationService().createTicket(json);
-        } catch (DatabaseException e) {
+            final String title = json.get("title").getAsString();
+
+            final String stateStr = json.get("state").getAsString();
+            final Ticket.State state = Ticket.State.valueOf(stateStr);
+
+            builder = getAPI().createTicket()
+                    .setTitle(title)
+                    .setState(state);
+
+            JsonArray users = json.getAsJsonArray("users");
+            StreamSupport.stream(users.spliterator(), false)
+                    .map(JsonElement::getAsLong)
+                    .map(id -> {
+                        UserImpl user = getAPI().getUser(id);
+                        if (user == null)
+                            throw new IllegalArgumentException("Invalid user id: " + id);
+                        return user;
+                    })
+                    .forEach(builder::addUsers);
+
+            JsonArray tags = json.getAsJsonArray("tags");
+            StreamSupport.stream(tags.spliterator(), false)
+                    .map(JsonElement::getAsString)
+                    .forEach(builder::addTags);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(e.getMessage());
+        } catch (Exception e) {
+            throw new BadRequestException("Malformed body");
+        }
+
+        try {
+            result = ((TicketImpl) builder.await()).toJson();
+        } catch (Exception e) {
             throw new InternalServerErrorException();
         }
 
@@ -70,12 +104,18 @@ public class TicketHandler {
     }
 
     public void onAddUser(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
-        final long ticket = request.getSegmentLong(1);
-        final long user   = request.getSegmentLong(3);
+        final long ticketId = request.getSegmentLong(1);
+        final long   userId = request.getSegmentLong(3);
+
+        final TicketImpl ticket = getAPI().getTicket(ticketId);
+        final UserImpl   user   = getAPI().getUser(userId);
+
+        if (ticket == null || user == null)
+            throw new NotFoundException();
 
         try {
-            getModerationService().addTicketUser(ticket, user);
-        } catch (DatabaseException e) {
+            ticket.addUser(user).await();
+        } catch (Exception e) {
             throw new InternalServerErrorException();
         }
 
@@ -83,24 +123,43 @@ public class TicketHandler {
     }
 
     public void onDeleteUser(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
-        final long ticket = request.getSegmentLong(1);
-        final long user   = request.getSegmentLong(3);
+        final long ticketId = request.getSegmentLong(1);
+        final long   userId = request.getSegmentLong(3);
 
-        try {
-            getModerationService().removeTicketUser(ticket, user);
-        } catch (DatabaseException e) {
-            throw new InternalServerErrorException();
+        final TicketImpl ticket = getAPI().getTicket(ticketId);
+        final UserImpl   user   = getAPI().getUser(userId);
+
+        if (ticket == null)
+            throw new NotFoundException();
+
+        if (user != null && ticket.getUsers().containsId(userId)) {
+            try {
+                ticket.removeUser(user).await();
+            } catch (Exception e) {
+                throw new InternalServerErrorException();
+            }
         }
 
         response.setCode(204);
     }
 
     public void onDelete(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
-        response.setCode(501);
+        final long ticketId = request.getSegmentLong(1);
+        final TicketImpl ticket = getAPI().getTicket(ticketId);
+
+        if (ticket != null) {
+            try {
+                ticket.delete().await();
+            } catch (Exception e) {
+                throw new InternalServerErrorException();
+            }
+        }
+
+        response.setCode(204);
     }
 
     public void onEdit(@NotNull Request request, @NotNull ResponseBuilder response) throws APIException {
-        long ticket = request.getSegmentLong(1);
+        final long ticketId = request.getSegmentLong(1);
 
         Optional<JsonObject> body = request.optionalBody()
                 .map(JsonElement::getAsJsonObject);
@@ -109,21 +168,58 @@ public class TicketHandler {
             throw new BadRequestException("No content");
 
         final JsonObject json = body.get();
-        final JsonObject result;
+
+        TicketImpl ticket = getAPI().getTicket(ticketId);
+        if (ticket == null)
+            throw new NotFoundException();
+        final TicketModifierImpl modifier = ticket.modify();
 
         try {
-            getModerationService().patchTicket(ticket, json);
+            Optional.ofNullable(json.get("title"))
+                    .map(JsonElement::getAsString)
+                    .ifPresent(modifier::setTitle);
 
-            result = getModerationService().getTicket(ticket);
-        } catch (DatabaseException e) {
+            Optional.ofNullable(json.get("state"))
+                    .map(JsonElement::getAsString)
+                    .map(Ticket.State::valueOf)
+                    .ifPresent(modifier::setState);
+
+            final List<String> tags = ticket.getTags();
+            Optional.ofNullable(json.get("tags"))
+                    .map(JsonElement::getAsJsonArray)
+                    .map(elements -> {
+                        List<String> tagStrings = new ArrayList<>(elements.size());
+                        for (JsonElement element : elements)
+                            tagStrings.add(element.getAsString());
+                        return tagStrings;
+                    })
+                    .ifPresent(elements -> {
+                        // add new tags
+                        for (String element : elements)
+                            if (!tags.contains(element))
+                                modifier.addTags(element);
+                        // remove old tags
+                        for (String element : tags)
+                            if (!elements.contains(element))
+                                modifier.removeTags(element);
+                    });
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(e.getMessage());
+        } catch (Exception e) {
+            throw new BadRequestException("Malformed body");
+        }
+
+        try {
+            ticket = ((TicketImpl) modifier.await());
+        } catch (Exception e) {
             throw new InternalServerErrorException();
         }
 
         response.setCode(200);
-        response.setBody(result);
+        response.setBody(ticket.toJson());
     }
 
-    private @NotNull ModerationService getModerationService() {
-        return this.networkHandler.getServer().getModerationService();
+    private @NotNull ObeliskImpl getAPI() {
+        return this.networkHandler.getServer().getAPI();
     }
 }
