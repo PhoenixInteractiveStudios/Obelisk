ba38939d3a5a751ecea6c69f2c77caa78639af56
TurtleException <turtle.exception@eldritch.de>
2024-01-18 06:12:46 +0100

    Delete redundant entity services

--- obelisk-server/src/main/java/org/burrow_studios/obelisk/server/ObeliskServer.java
+++ obelisk-server/src/main/java/org/burrow_studios/obelisk/server/ObeliskServer.java
@@ -7,11 +7,8 @@ import org.burrow_studios.obelisk.core.net.socket.NetworkException;
 import org.burrow_studios.obelisk.server.auth.Authenticator;
 import org.burrow_studios.obelisk.server.db.EntityProvider;
 import org.burrow_studios.obelisk.server.event.EventManager;
-import org.burrow_studios.obelisk.server.its.IssueTracker;
-import org.burrow_studios.obelisk.server.moderation.ModerationService;
 import org.burrow_studios.obelisk.server.net.NetworkHandler;
 import org.burrow_studios.obelisk.server.permissions.PermissionManager;
-import org.burrow_studios.obelisk.server.users.UserService;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -20,11 +17,8 @@ public final class ObeliskServer {
     private final Authenticator     authenticator;
     private final EntityProvider    entityProvider;
     private final EventManager      eventManager;
-    private final IssueTracker      issueTracker;
-    private final ModerationService moderationService;
     private final NetworkHandler    networkHandler;
     private final PermissionManager permissionManager;
-    private final UserService       userService;
 
     private final ObeliskImpl api;
 
@@ -32,11 +26,8 @@ public final class ObeliskServer {
         this.authenticator     = new Authenticator(this);
         this.entityProvider    = new EntityProvider(this);
         this.eventManager      = new EventManager(this);
-        this.issueTracker      = new IssueTracker(this);
-        this.moderationService = new ModerationService(this);
         this.networkHandler    = new NetworkHandler(this);
         this.permissionManager = new PermissionManager(this);
-        this.userService       = new UserService(this);
 
         this.api = ((ObeliskBuilderImpl) ObeliskBuilder.create())
                 .setDataProvider(obelisk -> entityProvider)
@@ -59,24 +50,12 @@ public final class ObeliskServer {
         return eventManager;
     }
 
-    public @NotNull IssueTracker getIssueTracker() {
-        return issueTracker;
-    }
-
-    public @NotNull ModerationService getModerationService() {
-        return moderationService;
-    }
-
     public @NotNull NetworkHandler getNetworkHandler() {
         return networkHandler;
     }
 
     public @NotNull PermissionManager getPermissionManager() {
         return permissionManager;
-    }
-
-    public @NotNull UserService getUserService() {
-        return userService;
     }
 
     public @NotNull ObeliskImpl getAPI() {
