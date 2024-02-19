9e44d448c20be34b5c1714e36d27596418bf1872
TurtleException <turtle.exception@eldritch.de>
2024-02-19 18:57:25 +0100

    Update documentation of commons/http

--- obelisk-commons/src/main/java/org/burrow_studios/obelisk/commons/http/HTTPResponse.java
+++ obelisk-commons/src/main/java/org/burrow_studios/obelisk/commons/http/HTTPResponse.java
@@ -1,10 +1,20 @@
 package org.burrow_studios.obelisk.commons.http;
 
+import org.burrow_studios.obelisk.commons.http.client.HTTPClient;
+import org.burrow_studios.obelisk.commons.http.server.HTTPServer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
+/**
+ * An immutable representation of an HTTP response. In the context of an {@link HTTPClient} this represents the response
+ * from the remote server. In the context of an {@link HTTPServer} this represents the response that is sent to the
+ * remote client.
+ * @param code The HTTP status code.
+ * @param headers The HTTP response headers.
+ * @param body The HTTP response body (may be null).
+ */
 public record HTTPResponse(
         int code,
         @NotNull Map<String, String> headers,
