de7ca0b57fbc89b66f4933791d56a2d52be62572
TurtleException <turtle.exception@eldritch.de>
2024-02-23 17:53:50 +0100

    Create module 'obelisk-gateway'

---
+++ obelisk-gateway/src/main/java/org/burrow_studios/obelisk/gateway/Main.java
@@ -0,0 +1,48 @@
+package org.burrow_studios.obelisk.gateway;
+
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.gateway.util.logging.LogUtil;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class Main {
+    static {
+        System.out.print("Starting Obelisk Server");
+    }
+
+    /** Static application version. */
+    public static final String VERSION = ResourceTools.get(Main.class).getVersion();
+
+    /** Directory in which the JAR ist located. */
+    public static final File DIR;
+    static {
+        File f;
+        try {
+            f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to declare directory");
+            throw new RuntimeException(e);
+        }
+        DIR = f;
+    }
+
+    /** JVM entrypoint */
+    public static void main(String[] args) throws Exception {
+        if (VERSION == null)
+            throw new AssertionError("Unknown version");
+        System.out.printf(" version %s...%n", VERSION);
+
+        LogUtil.init();
+
+        ObeliskGateway gateway = new ObeliskGateway();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                gateway.stop();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+    }
+}
