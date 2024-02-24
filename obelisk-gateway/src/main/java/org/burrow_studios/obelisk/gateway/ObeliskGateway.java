57c7dde1cabdee5cb1f74f55350da8488b70035d
TurtleException <turtle.exception@eldritch.de>
2024-02-24 16:03:32 +0100

    Create gateway config

--- obelisk-gateway/src/main/java/org/burrow_studios/obelisk/gateway/ObeliskGateway.java
+++ obelisk-gateway/src/main/java/org/burrow_studios/obelisk/gateway/ObeliskGateway.java
@@ -1,11 +1,25 @@
 package org.burrow_studios.obelisk.gateway;
 
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.burrow_studios.obelisk.commons.yaml.YamlSection;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ObeliskGateway {
-    ObeliskGateway() {
-        // TODO
+    private final @NotNull YamlSection config;
+    private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
+
+    ObeliskGateway() throws IOException {
+        ResourceTools resourceTools = ResourceTools.get(Main.class);
+        YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
+
+        this.config = YamlUtil.load(configFile, YamlSection.class);
     }
 
-    void stop() {
-        // TODO
+    void stop() throws IOException {
+        this.config.save(configFile);
     }
 }
