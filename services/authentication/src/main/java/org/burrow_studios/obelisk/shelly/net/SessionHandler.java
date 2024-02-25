02cc57d938da9bf1d888e8097bcdd178501c4cb8
TurtleException <turtle.exception@eldritch.de>
2024-02-25 14:08:39 +0100

    Fix Shelly SessionHandler

--- services/authentication/src/main/java/org/burrow_studios/obelisk/shelly/net/SessionHandler.java
+++ services/authentication/src/main/java/org/burrow_studios/obelisk/shelly/net/SessionHandler.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonObject;
 import org.burrow_studios.obelisk.commons.rpc.RPCRequest;
 import org.burrow_studios.obelisk.commons.rpc.RPCResponse;
 import org.burrow_studios.obelisk.commons.rpc.Status;
-import org.burrow_studios.obelisk.commons.rpc.exceptions.ForbiddenException;
+import org.burrow_studios.obelisk.commons.rpc.exceptions.BadRequestException;
 import org.burrow_studios.obelisk.commons.rpc.exceptions.RequestHandlerException;
 import org.burrow_studios.obelisk.shelly.Shelly;
 import org.burrow_studios.obelisk.shelly.crypto.TokenManager;
@@ -19,15 +19,14 @@ public class SessionHandler {
     }
 
     public void onLogin(@NotNull RPCRequest request, @NotNull RPCResponse.Builder response) throws RequestHandlerException {
-        // this method should not be invoked without checking authorization first
-        assert request.endpoint().getPrivilege() == AuthLevel.IDENTITY;
-        assert request.token() != null;
-
         final TokenManager tokenManager = getTokenManager();
 
         final String subjectStr = request.getPath().split("/")[1];
         final long   subject    = Long.parseLong(subjectStr);
-        final long   identity   = Long.parseLong(request.token().getId());
+
+        if (!(request.getBody() instanceof JsonObject requestBody))
+            throw new BadRequestException("Missing request body");
+        long identity = requestBody.get("identity").getAsLong();
 
         final String sessionToken = tokenManager.newSessionToken(identity, subject);
 
@@ -39,20 +38,14 @@ public class SessionHandler {
     }
 
     public void onLogout(@NotNull RPCRequest request, @NotNull RPCResponse.Builder response) throws RequestHandlerException, DatabaseException {
-        // this method should not be invoked without checking authorization first
-        assert request.endpoint().getPrivilege() == AuthLevel.SESSION;
-        assert request.token() != null;
-
         final String[] pathSegments = request.getPath().split("/");
 
-        final long subject  = Long.parseLong(pathSegments[1]);
-        final long session  = Long.parseLong(pathSegments[2]);
-        final long identity = Long.parseLong(request.token().getKeyId());
+        final long subject = Long.parseLong(pathSegments[1]);
+        final long session = Long.parseLong(pathSegments[2]);
 
-        // validate subject
-        final long tokenSubject = Long.parseLong(request.token().getSubject());
-        if (subject != tokenSubject)
-            throw new ForbiddenException("Mismatch between token subject and requested subject resource. You may only access your own sessions.");
+        if (!(request.getBody() instanceof JsonObject requestBody))
+            throw new BadRequestException("Missing request body");
+        long identity = requestBody.get("identity").getAsLong();
 
         this.shelly.getDatabase().invalidateSession(session, identity);
 
@@ -60,18 +53,12 @@ public class SessionHandler {
     }
 
     public void onLogoutAll(@NotNull RPCRequest request, @NotNull RPCResponse.Builder response) throws RequestHandlerException {
-        // this method should not be invoked without checking authorization first
-        assert request.endpoint().getPrivilege() == AuthLevel.SESSION;
-        assert request.token() != null;
-
         final String subjectStr = request.getPath().split("/")[1];
         final long   subject    = Long.parseLong(subjectStr);
-        final long   identity   = Long.parseLong(request.token().getKeyId());
 
-        // validate subject
-        final long tokenSubject = Long.parseLong(request.token().getSubject());
-        if (subject != tokenSubject)
-            throw new ForbiddenException("Mismatch between token subject and requested subject resource. You may only access your own sessions.");
+        if (!(request.getBody() instanceof JsonObject requestBody))
+            throw new BadRequestException("Missing request body");
+        long identity = requestBody.get("identity").getAsLong();
 
         this.shelly.getDatabase().invalidateAllSessions(identity);
 
