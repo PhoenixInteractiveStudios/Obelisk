acc55950af97764fb0ba8dd7d4837ea336bd7636
TurtleException <turtle.exception@eldritch.de>
2024-02-25 20:08:28 +0100

    Implement Chramel main classes

---
+++ services/authorization/src/main/java/org/burrow_studios/obelisk/chramel/Chramel.java
@@ -0,0 +1,31 @@
+package org.burrow_studios.obelisk.chramel;
+
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.commons.yaml.YamlSection;
+import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Chramel {
+    private static final Logger LOG = Logger.getLogger("MAIN");
+
+    private final @NotNull YamlSection config;
+    private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
+
+
+    Chramel() throws Exception {
+        ResourceTools resourceTools = ResourceTools.get(Main.class);
+        YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
+
+        this.config = YamlUtil.load(configFile, YamlSection.class);
+    }
+
+    void stop() throws Exception {
+        LOG.log(Level.WARNING, "Shutting down");
+        this.config.save(configFile);
+        LOG.log(Level.INFO, "OK bye");
+    }
+}
