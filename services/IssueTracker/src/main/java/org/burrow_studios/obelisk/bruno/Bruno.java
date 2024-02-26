7d2d72f509a959b1235130616ee715893df5eaee
TurtleException <turtle.exception@eldritch.de>
2024-02-26 15:02:54 +0100

    Create Bruno main classes

---
+++ services/IssueTracker/src/main/java/org/burrow_studios/obelisk/bruno/Bruno.java
@@ -0,0 +1,15 @@
+package org.burrow_studios.obelisk.bruno;
+
+import java.util.logging.Logger;
+
+public class Bruno {
+    private static final Logger LOG = Logger.getLogger("MAIN");
+
+    Bruno() throws Exception {
+
+    }
+
+    void stop() throws Exception {
+
+    }
+}
