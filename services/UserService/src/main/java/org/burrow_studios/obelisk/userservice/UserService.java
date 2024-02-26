2345760296113f6903df95d7cc2a562709963725
TurtleException <turtle.exception@eldritch.de>
2024-02-26 13:28:01 +0100

    Implement UserService config

--- services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/UserService.java
+++ services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/UserService.java
@@ -1,11 +1,31 @@
 package org.burrow_studios.obelisk.userservice;
 
-public class UserService {
-    UserService() {
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.commons.yaml.YamlSection;
+import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserService {
+    private static final Logger LOG = Logger.getLogger("MAIN");
+
+    private final @NotNull YamlSection config;
+    private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
+
+    UserService() throws IOException {
+        ResourceTools resourceTools = ResourceTools.get(Main.class);
+        YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
+
+        this.config = YamlUtil.load(configFile, YamlSection.class);
     }
 
     void stop() throws Exception {
-
+        LOG.log(Level.WARNING, "Shutting down");
+        this.config.save(configFile);
+        LOG.log(Level.INFO, "OK bye");
     }
 }
