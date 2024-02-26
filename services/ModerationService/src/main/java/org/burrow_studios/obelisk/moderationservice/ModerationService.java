38d4b4997eb37bd9c30c12ee9abceae05bf09d0b
TurtleException <turtle.exception@eldritch.de>
2024-02-26 21:36:35 +0100

    Add ModerationService config

--- services/ModerationService/src/main/java/org/burrow_studios/obelisk/moderationservice/ModerationService.java
+++ services/ModerationService/src/main/java/org/burrow_studios/obelisk/moderationservice/ModerationService.java
@@ -1,17 +1,30 @@
 package org.burrow_studios.obelisk.moderationservice;
 
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.commons.yaml.YamlSection;
+import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ModerationService {
     private static final Logger LOG = Logger.getLogger("MAIN");
 
-    ModerationService() throws Exception {
+    private final @NotNull YamlSection config;
+    private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
 
+    ModerationService() throws Exception {
+        ResourceTools resourceTools = ResourceTools.get(Main.class);
+        YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
+
+        this.config = YamlUtil.load(configFile, YamlSection.class);
     }
 
     void stop() throws Exception {
         LOG.log(Level.WARNING, "Shutting down");
+        this.config.save(configFile);
         LOG.log(Level.INFO, "OK bye");
     }
 }
