af27276deeacc287072378cae02e5fc833370b4b
TurtleException <turtle.exception@eldritch.de>
2024-02-26 13:25:16 +0100

    Create UserService main classes

---
+++ services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/Main.java
@@ -0,0 +1,48 @@
+package org.burrow_studios.obelisk.userservice;
+
+import org.burrow_studios.obelisk.commons.logging.LogTools;
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class Main {
+    static {
+        System.out.print("Starting Shelly");
+    }
+
+    /** Static application version. */
+    public static final String VERSION = ResourceTools.get(Main.class).getVersion();
+
+    /** Directory in which the JAR ist located. */
+    public static final File DIR;
+    static {
+        File f;
+        try {
+            f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to declare directory");
+            throw new RuntimeException(e);
+        }
+        DIR = f;
+    }
+
+    /** JVM entrypoint */
+    public static void main(String[] args) throws Exception {
+        if (VERSION == null)
+            throw new AssertionError("Unknown version");
+        System.out.printf(" version %s...%n", VERSION);
+
+        LogTools.get(new File(Main.DIR, "logs")).init();
+
+        UserService service = new UserService();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                service.stop();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+    }
+}
---
+++ services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/UserService.java
@@ -0,0 +1,11 @@
+package org.burrow_studios.obelisk.userservice;
+
+public class UserService {
+    UserService() {
+
+    }
+
+    void stop() throws Exception {
+
+    }
+}
