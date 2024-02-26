dca4a584b6c2a0cb13d8fd165209152503b1ab4f
TurtleException <turtle.exception@eldritch.de>
2024-02-26 15:06:30 +0100

    Add Bruno config

--- services/IssueTracker/src/main/java/org/burrow_studios/obelisk/bruno/Bruno.java
+++ services/IssueTracker/src/main/java/org/burrow_studios/obelisk/bruno/Bruno.java
@@ -1,15 +1,30 @@
 package org.burrow_studios.obelisk.bruno;
 
+import org.burrow_studios.obelisk.commons.util.ResourceTools;
+import org.burrow_studios.obelisk.commons.yaml.YamlSection;
+import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Bruno {
     private static final Logger LOG = Logger.getLogger("MAIN");
 
-    Bruno() throws Exception {
+    private final @NotNull YamlSection config;
+    private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
 
+    Bruno() throws Exception {
+        ResourceTools resourceTools = ResourceTools.get(Main.class);
+        YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
+
+        this.config = YamlUtil.load(configFile, YamlSection.class);
     }
 
     void stop() throws Exception {
-
+        LOG.log(Level.WARNING, "Shutting down");
+        this.config.save(configFile);
+        LOG.log(Level.INFO, "OK bye");
     }
 }
