0e93eba781258aa9357fe84d4d70eba887fa19b1
TurtleException <turtle.exception@eldritch.de>
2024-02-26 13:32:59 +0100

    Implement UserService database

--- services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/UserService.java
+++ services/UserService/src/main/java/org/burrow_studios/obelisk/userservice/UserService.java
@@ -3,6 +3,9 @@ package org.burrow_studios.obelisk.userservice;
 import org.burrow_studios.obelisk.commons.util.ResourceTools;
 import org.burrow_studios.obelisk.commons.yaml.YamlSection;
 import org.burrow_studios.obelisk.commons.yaml.YamlUtil;
+import org.burrow_studios.obelisk.userservice.database.EntityDatabase;
+import org.burrow_studios.obelisk.userservice.database.GroupDB;
+import org.burrow_studios.obelisk.userservice.database.UserDB;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -16,16 +19,37 @@ public class UserService {
     private final @NotNull YamlSection config;
     private final @NotNull File configFile = new File(Main.DIR, "config.yaml");
 
+    private final EntityDatabase database;
+
     UserService() throws IOException {
         ResourceTools resourceTools = ResourceTools.get(Main.class);
         YamlUtil.saveDefault(configFile, resourceTools.getResource("config.yaml"));
 
         this.config = YamlUtil.load(configFile, YamlSection.class);
+
+        YamlSection dbConfig = this.config.getAsSection("db");
+
+        this.database = new EntityDatabase(
+                dbConfig.getAsPrimitive("host").getAsString(),
+                dbConfig.getAsPrimitive("port").getAsInt(),
+                dbConfig.getAsPrimitive("database").getAsString(),
+                dbConfig.getAsPrimitive("user").getAsString(),
+                dbConfig.getAsPrimitive("pass").getAsString()
+        );
     }
 
     void stop() throws Exception {
         LOG.log(Level.WARNING, "Shutting down");
+        this.database.close();
         this.config.save(configFile);
         LOG.log(Level.INFO, "OK bye");
+    }
+
+    public @NotNull GroupDB getGroupDB() {
+        return this.database;
+    }
+
+    public @NotNull UserDB getUserDB() {
+        return this.database;
     }
 }
