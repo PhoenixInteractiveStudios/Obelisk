c1e2b46a056ca9f999a3e7d9f424637a5e40ae7d
TurtleException <turtle.exception@eldritch.de>
2024-05-16 00:36:09 +0200

    Provide access to entity caches via API

--- obelisk-api/src/main/java/org/burrow_studios/obelisk/api/Obelisk.java
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/api/Obelisk.java
@@ -5,6 +5,10 @@ import org.burrow_studios.obelisk.api.action.entity.minecraft.DiscordAccountBuil
 import org.burrow_studios.obelisk.api.action.entity.project.ProjectBuilder;
 import org.burrow_studios.obelisk.api.action.entity.ticket.TicketBuilder;
 import org.burrow_studios.obelisk.api.action.entity.user.UserBuilder;
+import org.burrow_studios.obelisk.api.cache.EntitySet;
+import org.burrow_studios.obelisk.api.entities.Project;
+import org.burrow_studios.obelisk.api.entities.Ticket;
+import org.burrow_studios.obelisk.api.entities.User;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
@@ -18,6 +22,12 @@ public interface Obelisk {
     void awaitShutdown() throws InterruptedException;
 
     void awaitShutdown(long timeout, @NotNull TimeUnit unit) throws InterruptedException, TimeoutException;
+
+    @NotNull EntitySet<? extends User> getUsers();
+
+    @NotNull EntitySet<? extends Ticket> getTickets();
+
+    @NotNull EntitySet<? extends Project> getProjects();
 
     @NotNull UserBuilder createUser();
 
