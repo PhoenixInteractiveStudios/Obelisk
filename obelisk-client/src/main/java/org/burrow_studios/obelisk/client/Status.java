222cb806a2c3b9b9a47d51aeed103e7867520313
TurtleException <turtle.exception@eldritch.de>
2024-05-17 00:32:33 +0200

    Create client status

---
+++ obelisk-client/src/main/java/org/burrow_studios/obelisk/client/Status.java
@@ -0,0 +1,14 @@
+package org.burrow_studios.obelisk.client;
+
+public enum Status {
+    /** Local initialization. Network connections have yet to be established. */
+    PRE_INIT,
+    /** Handshaking with server, establishing socket connection and building caches. */
+    INIT,
+    /** Ready to process requests and handle events. */
+    READY,
+    /** Received stop command; Awaiting pending requests & terminating connections. */
+    STOPPING,
+    /** All connection closed; Request cannot be processed anymore. */
+    STOPPED
+}
