fe51924ab5ba9230bde80a91a7feec93700e3544
TurtleException <turtle.exception@eldritch.de>
2024-05-17 02:14:59 +0200

    Create monolith main classes

---
+++ obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/Main.java
@@ -0,0 +1,8 @@
+package org.burrow_studios.obelisk.monolith;
+
+public class Main {
+    /** JVM entrypoint */
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/ObeliskMonolith.java
@@ -0,0 +1,12 @@
+package org.burrow_studios.obelisk.monolith;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ObeliskMonolith {
+    private static final Logger LOG = LoggerFactory.getLogger(ObeliskMonolith.class);
+
+    public ObeliskMonolith() {
+
+    }
+}
