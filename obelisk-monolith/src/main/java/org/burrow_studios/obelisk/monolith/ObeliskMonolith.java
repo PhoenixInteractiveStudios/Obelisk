bac09d6934ae73d472ce6cbd79a67dd03cebb984
TurtleException <turtle.exception@eldritch.de>
2024-05-20 20:55:06 +0200

    Make ObeliskMonolith extend AbstractObelisk

--- obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/ObeliskMonolith.java
+++ obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/ObeliskMonolith.java
@@ -1,12 +1,49 @@
 package org.burrow_studios.obelisk.monolith;
 
+import org.burrow_studios.obelisk.api.action.entity.discord.MinecraftAccountBuilder;
+import org.burrow_studios.obelisk.api.action.entity.minecraft.DiscordAccountBuilder;
+import org.burrow_studios.obelisk.api.action.entity.project.ProjectBuilder;
+import org.burrow_studios.obelisk.api.action.entity.ticket.TicketBuilder;
+import org.burrow_studios.obelisk.api.action.entity.user.UserBuilder;
+import org.burrow_studios.obelisk.core.AbstractObelisk;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ObeliskMonolith {
+public class ObeliskMonolith extends AbstractObelisk {
     private static final Logger LOG = LoggerFactory.getLogger(ObeliskMonolith.class);
 
     public ObeliskMonolith() {
+        super();
+    }
 
+    @Override
+    public @NotNull UserBuilder createUser() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public @NotNull TicketBuilder createTicket() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public @NotNull ProjectBuilder createProject() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public @NotNull DiscordAccountBuilder createDiscordAccount() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public @NotNull MinecraftAccountBuilder createMinecraftAccount() {
+        // TODO
+        return null;
     }
 }
