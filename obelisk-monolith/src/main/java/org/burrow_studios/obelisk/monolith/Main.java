944325ab423a050a1ac17b0ddb31bff41995b014
TurtleException <turtle.exception@eldritch.de>
2024-05-23 22:52:41 +0200

    Create Main.DIR

--- obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/Main.java
+++ obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/Main.java
@@ -1,6 +1,22 @@
 package org.burrow_studios.obelisk.monolith;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class Main {
+    /** Directory in which the JAR ist located. */
+    public static final File DIR;
+    static {
+        File f;
+        try {
+            f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to declare directory");
+            throw new RuntimeException(e);
+        }
+        DIR = f;
+    }
+
     /** JVM entrypoint */
     public static void main(String[] args) {
         // TODO
