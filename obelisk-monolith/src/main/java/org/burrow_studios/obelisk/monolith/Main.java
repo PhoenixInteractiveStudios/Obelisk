a97ded1e721b23d24b9fd0c6afca00e28bc62fbe
TurtleException <turtle.exception@eldritch.de>
2024-05-23 22:58:44 +0200

    Create VERSION constant for monolith

--- obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/Main.java
+++ obelisk-monolith/src/main/java/org/burrow_studios/obelisk/monolith/Main.java
@@ -1,11 +1,17 @@
 package org.burrow_studios.obelisk.monolith;
 
-import org.burrow_studios.obelisk.monolith.exceptions.DatabaseException;
+import org.burrow_studios.obelisk.util.ResourceTools;
 
 import java.io.File;
 import java.net.URISyntaxException;
 
 public class Main {
+    static {
+        System.out.print("Starting Obelisk Monolith");
+    }
+
+    public static final String VERSION = ResourceTools.get(Main.class).getVersion();
+
     /** Directory in which the JAR ist located. */
     public static final File DIR;
     static {
@@ -20,7 +26,11 @@ public class Main {
     }
 
     /** JVM entrypoint */
-    public static void main(String[] args) throws DatabaseException {
+    public static void main(String[] args) throws Exception {
+        if (VERSION == null)
+            throw new AssertionError("Unknown version");
+        System.out.printf(" version %s...%n", VERSION);
+
         ObeliskMonolith obelisk = new ObeliskMonolith();
     }
 }
