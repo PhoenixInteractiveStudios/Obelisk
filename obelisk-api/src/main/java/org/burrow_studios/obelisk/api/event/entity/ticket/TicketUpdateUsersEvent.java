63c1e2ff3365f77abcdd81d3ecf97f3f506b432e
TurtleException <turtle.exception@eldritch.de>
2024-05-25 22:13:20 +0200

    Merge TicketUserAddEvent & TicketUserRemoveEvent into TicketUpdateUsersEvent

---
+++ obelisk-api/src/main/java/org/burrow_studios/obelisk/api/event/entity/ticket/TicketUpdateUsersEvent.java
@@ -0,0 +1,32 @@
+package org.burrow_studios.obelisk.api.event.entity.ticket;
+
+import org.burrow_studios.obelisk.api.entities.Ticket;
+import org.burrow_studios.obelisk.api.entities.User;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TicketUpdateUsersEvent extends TicketUpdateEvent<Set<User>> {
+    private final @NotNull Set<User>   addedUsers;
+    private final @NotNull Set<User> removedUsers;
+
+    public TicketUpdateUsersEvent(long id, @NotNull Ticket entity, @NotNull Set<User> oldValue, @NotNull Set<User> newValue) {
+        super(id, entity, oldValue, newValue);
+
+        this.addedUsers = newValue.stream()
+                .filter(user -> !oldValue.contains(user))
+                .collect(Collectors.toUnmodifiableSet());
+        this.removedUsers = oldValue.stream()
+                .filter(user -> !newValue.contains(user))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    public @NotNull Set<User> getAddedUsers() {
+        return this.addedUsers;
+    }
+
+    public @NotNull Set<User> getRemovedUsers() {
+        return this.removedUsers;
+    }
+}
