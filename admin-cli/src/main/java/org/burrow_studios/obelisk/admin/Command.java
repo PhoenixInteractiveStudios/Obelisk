247a1a62f30c696c773e5f87e6c10b5576b18e0c
TurtleException <turtle.exception@eldritch.de>
2024-05-27 03:02:39 +0200

    Implement verbose logging

--- admin-cli/src/main/java/org/burrow_studios/obelisk/admin/Command.java
+++ admin-cli/src/main/java/org/burrow_studios/obelisk/admin/Command.java
@@ -1,5 +1,8 @@
 package org.burrow_studios.obelisk.admin;
 
+import ch.qos.logback.classic.Level;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
@@ -15,6 +18,14 @@ public class Command implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
+        if (verbose)
+            setDebug();
+
         return 0;
+    }
+    
+    private static void setDebug() {
+        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.setLevel(Level.DEBUG);
     }
 }
