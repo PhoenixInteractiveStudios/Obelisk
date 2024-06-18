c15ac9caf7a22f8474f56a9ccfab097022e4eee2
TurtleException <turtle.exception@eldritch.de>
2024-06-18 20:43:52 +0200

    Implement Ticket entity

---
+++ application/src/main/java/org/burrow_studios/obelkisk/db/interfaces/TicketDB.java
@@ -0,0 +1,19 @@
+package org.burrow_studios.obelkisk.db.interfaces;
+
+import org.burrow_studios.obelkisk.entity.Ticket;
+import org.burrow_studios.obelkisk.exceptions.DatabaseException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public interface TicketDB {
+    @NotNull List<Ticket> listTickets() throws DatabaseException;
+
+    @NotNull String getTicketTitle(long id) throws DatabaseException;
+
+    @NotNull Ticket createTicket(@NotNull String title) throws DatabaseException;
+
+    void setTicketTitle(long id, @NotNull String title) throws DatabaseException;
+
+    void deleteTicket(long id) throws DatabaseException;
+}
---
+++ application/src/main/java/org/burrow_studios/obelkisk/entity/Ticket.java
@@ -0,0 +1,26 @@
+package org.burrow_studios.obelkisk.entity;
+
+import org.burrow_studios.obelkisk.db.interfaces.TicketDB;
+import org.jetbrains.annotations.NotNull;
+
+public final class Ticket extends AbstractEntity {
+    private final TicketDB database;
+
+    public Ticket(long id, @NotNull TicketDB database) {
+        super(id);
+        this.database = database;
+    }
+
+    public @NotNull String getTitle() {
+        return this.database.getTicketTitle(this.id);
+    }
+
+    public void setTitle(@NotNull String title) {
+        this.database.setTicketTitle(this.id, title);
+    }
+
+    @Override
+    public void delete() {
+        this.database.deleteTicket(this.id);
+    }
+}
