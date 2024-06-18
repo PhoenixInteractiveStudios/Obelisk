71d5cc55c18457b525a2738f08e26d2c2b07abaa
TurtleException <turtle.exception@eldritch.de>
2024-06-18 14:37:12 +0200

    Delete core module

--- obelisk-core/src/main/java/org/burrow_studios/obelisk/core/http/Method.java
+++
@@ -1,9 +0,0 @@
-package org.burrow_studios.obelisk.core.http;
-
-public enum Method {
-    GET,
-    PUT,
-    POST,
-    PATCH,
-    DELETE
-}
--- obelisk-core/src/main/java/org/burrow_studios/obelisk/core/socket/Protocol.java
+++
@@ -1,7 +0,0 @@
-package org.burrow_studios.obelisk.core.socket;
-
-public final class Protocol {
-    public static final int VERSION = 1;
-
-    private Protocol() { }
-}
