612d3c95cc62db2f9e21556227f739c0490e38a5
TurtleException <turtle.exception@eldritch.de>
2024-06-18 15:21:47 +0200

    Create DIR constant

--- application/src/main/java/org/burrow_studios/obelkisk/Main.java
+++ application/src/main/java/org/burrow_studios/obelkisk/Main.java
@@ -2,12 +2,28 @@ package org.burrow_studios.obelkisk;
 
 import org.burrow_studios.obelisk.util.ResourceTools;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class Main {
     static {
         System.out.print("Starting Obelisk");
     }
 
     public static final String VERSION = ResourceTools.get(Main.class).getVersion();
+
+    /** Directory in which the JAR ist located. */
+    public static final File DIR;
+    static {
+        File f;
+        try {
+            f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to declare directory");
+            throw new RuntimeException(e);
+        }
+        DIR = f;
+    }
 
     public static void main(String[] args) {
         if (VERSION == null)
