2e0f1605908a3ea93a1ce073ca5beb7bbbf833dc
TurtleException <turtle.exception@eldritch.de>
2024-06-18 15:21:13 +0200

    Create VERSION constant

--- application/src/main/java/org/burrow_studios/obelkisk/Main.java
+++ application/src/main/java/org/burrow_studios/obelkisk/Main.java
@@ -1,7 +1,17 @@
 package org.burrow_studios.obelkisk;
 
-public class Main {
-    public static void main(String[] args) {
+import org.burrow_studios.obelisk.util.ResourceTools;
 
+public class Main {
+    static {
+        System.out.print("Starting Obelisk");
+    }
+
+    public static final String VERSION = ResourceTools.get(Main.class).getVersion();
+
+    public static void main(String[] args) {
+        if (VERSION == null)
+            throw new Error("Unknown version");
+        System.out.printf(" version %s...%n", VERSION);
     }
 }
