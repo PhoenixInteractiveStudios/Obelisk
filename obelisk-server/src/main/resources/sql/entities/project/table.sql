e58aec9d3a852df33830cc7ed48981a9cd11945b
TurtleException <turtle.exception@eldritch.de>
2024-07-08 22:50:42 +0200

    Fix typo in project table.sql

--- obelisk-server/src/main/resources/sql/entities/project/table.sql
+++ obelisk-server/src/main/resources/sql/entities/project/table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `projects` (
     `id` INTEGER PRIMARY KEY,
     `title` TEXT NOT NULL,
     `application_template` TEXT,
-    `invite_only` BOOLEAN NOT NULL,
+    `invite_only` BOOLEAN NOT NULL
 );
